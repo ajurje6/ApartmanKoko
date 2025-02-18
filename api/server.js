f772b5d838ce1d6e52e5c1eeb2a1c0aa675ca10d
ajurje6 <antola.lukic@gmail.com>
2025-02-18 10:54:22 +0100

    increased rate limit

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ app.use(express.json()); // Parse incoming JSON
 // Rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+  max: 40,
 });
 app.use(limiter);
 
